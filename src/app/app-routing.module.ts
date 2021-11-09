47fa37b3b483c4762ca5bcbfa28ace0712da492d
dang dinh phuong <dangxuong2000@gmail.com>
2021-11-10 01:51:35 +0700

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,115 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from '@angular/router';
-import{LayoutComponent} from'../app/screens/admin/layout/layout.component';
-import{DasboarchComponent} from'../app/screens/admin/dasboarch/dasboarch.component';
-import{MosterListComponent}from'../app/screens/admin/moster/moster-list/moster-list.component';
-import{MosterAddComponent}from'../app/screens/admin/moster/moster-add/moster-add.component';
-import{MosterEditComponent}from'../app/screens/admin/moster/moster-edit/moster-edit.component';
-import{NewCateComponent}from'../app/screens/admin/news/new-cate/new-cate.component';
-import{NewListComponent}from'../app/screens/admin/news/new-list/new-list.component';
-import{NewAddComponent}from'../app/screens/admin/news/new-add/new-add.component';
-import{NewEditComponent}from'../app/screens/admin/news/new-edit/new-edit.component';
-import{CateMosterListComponent}from'../app/screens/admin/moster/category/cate-moster-list/cate-moster-list.component';
-import{SuppliersListComponent}from'../app/screens/admin/moster/suppliers/suppliers-list/suppliers-list.component';
-import{LayoutCliComponent}from'../app/screens/client/layout-cli/layout-cli.component'
-import{HomeComponent}from'../app/screens/client/home/home.component'
-import{ShopComponent}from'../app/screens/client/shop/shop.component'
-import{BlogListComponent}from'../app/screens/client/blog/blog-list/blog-list.component'
-import{BlogDetailComponent}from'../app/screens/client/blog/blog-detail/blog-detail.component'
-import{ShopDetailComponent}from'../app/screens/client/shop-detail/shop-detail.component'
+
+import{LoginComponent}from'../app/screens/client/login/login.component'
+
 import { from } from 'rxjs';
 const routes: Routes = [ 
   {
-    path: "admin",
-    component: LayoutComponent,
-
-    children: [
-      {
-        path: "",
-        redirectTo: "dashboard",
-        pathMatch: "full"
-      },
-      {
-        path: "dashboard",
-        component: DasboarchComponent
-      },
-      {
-        path: "category/mosterList",
-        component: CateMosterListComponent
-      },
-      {
-        path: "suppliers/suppliersList",
-        component: SuppliersListComponent
-      },
-      {
-        path: "moster/mosterList",
-        component: MosterListComponent
-      },
-      {
-        path: "moster/mosterAdd",
-        component: MosterAddComponent
-      }
-     ,
-      {
-        path: "moster/mosterEdit/:id",
-        component: MosterEditComponent
-      },
-      {
-        path: "new/newcate",
-        component: NewCateComponent
-      },
-      
-      {
-        path: "new/newlist",
-        component: NewListComponent
-      },
-      {
-        path: "new/newAdd",
-        component: NewAddComponent
-      } ,
-      {
-        path: "new/newEdit/:id",
-        component: NewEditComponent
-      }
-     
-
-    ]
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full"
   },
   {
-    path:'',
-    component:LayoutCliComponent,
-    children: [
-      {
-        path: "",
-        redirectTo: "home",
-        pathMatch: "full"
-      },
-      {
-        path: "home",
-        component: HomeComponent
-      },
-      {
-        path: "shop",
-        component: ShopComponent
-      },
-      {
-        path: "shop/shopdetail/:id",
-        component: ShopDetailComponent
-      },
-      {
-        path: "blog/blogList/:id",
-        component: BlogListComponent
-      },
-      
-      {
-        path: "blog/blogdetail/:id",
-        component: BlogDetailComponent
-      }
-     
+    path: "login",
+    component: LoginComponent
+  },
 
-    ]
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,34 +11,15 @@ import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
 
 
 import { AppComponent } from './app.component';
-import { DasboarchComponent } from './screens/admin/dasboarch/dasboarch.component';
-import { MosterListComponent } from './screens/admin/moster/moster-list/moster-list.component';
-import { MosterAddComponent } from './screens/admin/moster/moster-add/moster-add.component';
-import { MosterEditComponent } from './screens/admin/moster/moster-edit/moster-edit.component';
-import { LayoutComponent } from './screens/admin/layout/layout.component';
-import {CateMosterListComponent} from'./screens/admin/moster/category/cate-moster-list/cate-moster-list.component';
-import { SuppliersListComponent } from './screens/admin/moster/suppliers/suppliers-list/suppliers-list.component';
-import { LayoutCliComponent } from './screens/client/layout-cli/layout-cli.component';
-import { HomeComponent } from './screens/client/home/home.component';
-import { ShopComponent } from './screens/client/shop/shop.component';
-import { ShopDetailComponent } from './screens/client/shop-detail/shop-detail.component';
-import { NewCateComponent } from './screens/admin/news/new-cate/new-cate.component';
-import { NewListComponent } from './screens/admin/news/new-list/new-list.component';
-import { NewAddComponent } from './screens/admin/news/new-add/new-add.component';
-import { NewEditComponent } from './screens/admin/news/new-edit/new-edit.component';
-import { BlogListComponent } from './screens/client/blog/blog-list/blog-list.component';
-import { BlogDetailComponent } from './screens/client/blog/blog-detail/blog-detail.component';
+
+
+import { LoginComponent } from './screens/client/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DasboarchComponent,
-    MosterListComponent,
-    MosterAddComponent,
-    MosterEditComponent,
-    LayoutComponent, 
-    CateMosterListComponent, SuppliersListComponent, LayoutCliComponent, HomeComponent, ShopComponent, ShopDetailComponent, NewCateComponent, NewListComponent, NewAddComponent, NewEditComponent, BlogListComponent, BlogDetailComponent, 
+  LoginComponent, 
   ],
   imports: [
     FormsModule,
